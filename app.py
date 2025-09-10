e3b3981e9dcb87c221b5977f1b628655b7ba1632
Adrian506-coder <treviadrian18@gmail.com>
2025-09-10 01:45:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -118,19 +118,20 @@ def guardarTraje():
 
 @app.route("/trajes/lista", methods=["GET"])
 def listarTrajes():
-    try:
-        con = get_connection()
-        cursor = con.cursor(dictionary=True)
-        cursor.execute("""
-            SELECT IdTraje, nombreTraje, descripcion
-            FROM trajes
-            ORDER BY IdTraje DESC
-        """)
-        registros = cursor.fetchall()
-        con.close()
-        return jsonify(registros)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    if not con.is_connected():
+        con.reconnect()
+        
+    cursor = con.cursor(dictionary=True)
+    cursor.execute("""
+        SELECT IdTraje, nombreTraje, descripcion
+        FROM trajes
+        ORDER BY IdTraje DESC
+    """)
+    registros = cursor.fetchall()
+    con.close()
+    
+    return make_response(jsonify(registros))
+
 
 
 
